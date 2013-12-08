ae72a81d342f7b2114045a3261fc0be2f37d1446
John Ihlein <jihlein@hotmail.com>
2013-12-07 19:42:14 -0700

    20131207 - PID D term low pass filter

--- src/pid.c
+++ src/pid.c
@@ -1,11 +1,11 @@
 /*
-  December 2012
+  Sept 2013
 
-  aq32Plus_F3 Rev -
+  bgc32 Rev -
 
-  Copyright (c) 2012 John Ihlein.  All rights reserved.
+  Copyright (c) 2013 John Ihlein.  All rights reserved.
 
-  Open Source STM32 Based Multicopter Controller Software
+  Open Source STM32 Based Brushless Gimbal Controller Software
 
   Includes code and/or ideas from:
 
@@ -15,9 +15,8 @@
   4)MultiWii
   5)S.O.H. Madgwick
   6)UAVX
-  7)STM DiscoveryF3 demonstration software
 
-  Designed to run on the DiscoveryF3 board
+  Designed to run on the EvvGC Brushless Gimbal Controller Board
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -41,19 +40,25 @@
 
 uint8_t holdIntegrators = true;
 
+#define F_CUT 20.0f
+
+static float rc;
+
 ///////////////////////////////////////////////////////////////////////////////
 
 void initPID(void)
 {
     uint8_t index;
 
+    rc = 1.0f / ( TWO_PI * F_CUT );
+
     for (index = 0; index < NUMBER_OF_PIDS; index++)
     {
-        eepromConfig.PID[index].iTerm          = 0.0f;
-        eepromConfig.PID[index].lastDcalcValue = 0.0f;
-        eepromConfig.PID[index].lastDterm      = 0.0f;
-        eepromConfig.PID[index].lastLastDterm  = 0.0f;
-    }
+    	eepromConfig.PID[index].iTerm          = 0.0f;
+    	eepromConfig.PID[index].lastDcalcValue = 0.0f;
+    	eepromConfig.PID[index].lastDterm      = 0.0f;
+    	eepromConfig.PID[index].lastLastDterm  = 0.0f;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -62,6 +67,7 @@ float updatePID(float command, float state, float deltaT, uint8_t iHold, struct 
 {
     float error;
     float dTerm;
+    float dTermFiltered;
     float dAverage;
 
     ///////////////////////////////////
@@ -75,44 +81,47 @@ float updatePID(float command, float state, float deltaT, uint8_t iHold, struct 
 
     if (iHold == false)
     {
-        PIDparameters->iTerm += error * deltaT;
-        PIDparameters->iTerm = constrain(PIDparameters->iTerm, -PIDparameters->windupGuard, PIDparameters->windupGuard);
+    	PIDparameters->iTerm += error * deltaT;
+    	PIDparameters->iTerm = constrain(PIDparameters->iTerm, -PIDparameters->windupGuard, PIDparameters->windupGuard);
     }
 
     ///////////////////////////////////
 
     if (PIDparameters->dErrorCalc == D_ERROR)  // Calculate D term from error
     {
-        dTerm = (error - PIDparameters->lastDcalcValue) / deltaT;
+		dTerm = (error - PIDparameters->lastDcalcValue) / deltaT;
         PIDparameters->lastDcalcValue = error;
-    }
-    else                                       // Calculate D term from state
-    {
-        dTerm = (PIDparameters->lastDcalcValue - state) / deltaT;
+	}
+	else                                       // Calculate D term from state
+	{
+		dTerm = (PIDparameters->lastDcalcValue - state) / deltaT;
 
-        if (PIDparameters->type == ANGULAR)
-            dTerm = standardRadianFormat(dTerm);
+		if (PIDparameters->type == ANGULAR)
+		    dTerm = standardRadianFormat(dTerm);
 
-        PIDparameters->lastDcalcValue = state;
-    }
+		PIDparameters->lastDcalcValue = state;
+	}
 
     ///////////////////////////////////
 
-    dAverage = (dTerm + PIDparameters->lastDterm + PIDparameters->lastLastDterm) * 0.333333f;
+    dTermFiltered = PIDparameters->lastDterm + deltaT / (rc + deltaT) * (dTerm - PIDparameters->lastDterm);
+
+    dAverage = (dTermFiltered + PIDparameters->lastDterm + PIDparameters->lastLastDterm) * 0.333333f;
 
     PIDparameters->lastLastDterm = PIDparameters->lastDterm;
-    PIDparameters->lastDterm = dTerm;
+    PIDparameters->lastDterm = dTermFiltered;
 
     ///////////////////////////////////
 
     if (PIDparameters->type == ANGULAR)
-        return (PIDparameters->P * error                +
-                PIDparameters->I * PIDparameters->iTerm +
-                PIDparameters->D * dAverage);
+        return(PIDparameters->P * error                +
+	           PIDparameters->I * PIDparameters->iTerm +
+	           PIDparameters->D * dAverage);
     else
-        return (PIDparameters->P * ((PIDparameters->B * command) - state) +
-                PIDparameters->I * PIDparameters->iTerm +
-                PIDparameters->D * dAverage);
+        return(PIDparameters->P * PIDparameters->B * command +
+               PIDparameters->I * PIDparameters->iTerm       +
+               PIDparameters->D * dAverage                   -
+               PIDparameters->P * state);
 
     ///////////////////////////////////
 }
@@ -121,7 +130,7 @@ float updatePID(float command, float state, float deltaT, uint8_t iHold, struct 
 
 void setPIDintegralError(uint8_t IDPid, float value)
 {
-    eepromConfig.PID[IDPid].iTerm = value;
+	eepromConfig.PID[IDPid].iTerm = value;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -131,7 +140,7 @@ void zeroPIDintegralError(void)
     uint8_t index;
 
     for (index = 0; index < NUMBER_OF_PIDS; index++)
-        setPIDintegralError(index, 0.0f);
+         setPIDintegralError(index, 0.0f);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -150,7 +159,7 @@ void zeroPIDstates(void)
     uint8_t index;
 
     for (index = 0; index < NUMBER_OF_PIDS; index++)
-        setPIDstates(index, 0.0f);
+         setPIDstates(index, 0.0f);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
